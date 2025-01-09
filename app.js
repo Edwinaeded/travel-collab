1c1e113111bd41305d6f4d48066097aaba093c6c
Edwinaeded <edwinaeded@gmail.com>
2025-01-09 15:28:48 +0800

    Build : project init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { create } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+const hbs = create({ extname: '.hbs' })
+
+app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`app is running on http://localhost:${port}`)
+})
+
+module.exports = app
