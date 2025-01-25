8d12b7094b214ac4d47c24ff454dc5504f097520
Edwinaeded <edwinaeded@gmail.com>
2025-01-25 18:14:17 +0800

    Feat : add destination model

---
+++ migrations/20250125095757-create-destination.js
@@ -0,0 +1,46 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Destinations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      start_time: {
+        type: Sequelize.TIME
+      },
+      end_time: {
+        type: Sequelize.TIME
+      },
+      cost: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Destinations')
+  }
+}
--- models/destination.js
+++ models/destination.js
@@ -3,27 +3,30 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Trip extends Model {
+  class Destination extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      Trip.hasMany(models.Destination, { foreignKey: 'tripId' })
+      Destination.belongsTo(models.Trip, { foreignKey: 'tripId' })
     }
   }
-  Trip.init({
+  Destination.init({
     name: DataTypes.STRING,
-    startDate: DataTypes.DATE,
-    endDate: DataTypes.DATE,
     description: DataTypes.TEXT,
+    date: DataTypes.DATE,
+    startTime: DataTypes.TIME,
+    endTime: DataTypes.TIME,
+    cost: DataTypes.STRING,
+    address: DataTypes.STRING,
     image: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Trip',
-    tableName: 'Trips',
+    modelName: 'Destination',
+    tableName: 'Destinations',
     underscored: true
   })
-  return Trip
+  return Destination
 }
