578d738fe1db3167d08e6de2ebf0a196819af743
Edwinaeded <edwinaeded@gmail.com>
2025-02-14 21:12:17 +0800

    Feat : add showing transportation time & distance & route by using Directions API

--- helpers/googleMaps-helper.js
+++ helpers/googleMaps-helper.js
@@ -14,18 +14,40 @@ const getGeoData = async function (address) {
     }
 
     const location = geoData.results[0].geometry.location
-    console.log('轉換經緯度完成：', location)
+    console.log('✅ 轉換經緯度完成：', location)
 
     return {
       latitude: location.lat,
       longitude: location.lng
     }
   } catch (err) {
-    console.error(`Geocoding API 請求失敗: ${err.message}`)
+    console.error(`❌ Geocoding API 請求失敗: ${err.message}`)
     throw new Error(`Geocoding API 請求失敗: ${err.message}`)
   }
 }
 
+const getGoogleMapsRoute = async function (req) {
+  try {
+    const params = {
+      origin: req.origin,
+      destination: req.destination,
+      key: process.env.GOOGLE_MAPS_API_KEY
+    }
+    if (req.waypoints && req.waypoints.length > 0) {
+      params.waypoints = req.waypoints.join('|')
+    }
+    const response = await axios.get('https://maps.googleapis.com/maps/api/directions/json', { params })
+    if (response.data.status !== 'OK') throw new Error(`路徑計算失敗: ${response.data.status}`)
+    console.log('✅ Directions API 計算路徑完成！')
+    console.log('✅ response.data:', response.data)
+    return response.data
+  } catch (err) {
+    console.error(`❌ Directions API 請求失敗: ${err.message}`)
+    throw new Error(`Directions API 請求失敗: ${err.message}`)
+  }
+}
+
 module.exports = {
-  getGeoData
+  getGeoData,
+  getGoogleMapsRoute
 }
--- services/trip-services.js
+++ services/trip-services.js
@@ -3,6 +3,7 @@ const { localFileHandler } = require('../helpers/file-helpers')
 const { getPagination } = require('../helpers/pagination-helpers')
 const { dayInterval, dayAdd } = require('../helpers/dayjs-helper')
 const { getUser } = require('../helpers/auth-helper')
+const { getGoogleMapsRoute } = require('../helpers/googleMaps-helper')
 
 const tripServices = {
   getTrips: (req, callback) => {
@@ -105,48 +106,80 @@ const tripServices = {
       .then(deletedTrip => callback(null, { deletedTrip }))
       .catch(err => callback(err))
   },
-  getTrip: (req, callback) => {
+  getTrip: async (req, callback) => {
     const { id } = req.params
     const currentDay = Number(req.query.day) || 1
     const user = getUser(req)
 
-    Trip.findByPk(id, {
-      include: [
-        { model: User, as: 'Sharers' },
-        { model: User, as: 'Receivers' }
-      ]
-    })
-      .then(tripData => {
-        if (!tripData) throw new Error("The trip doesn't exist!")
-        const trip = tripData.toJSON()
-        const isReceiver = trip.Receivers.some(r => r.id === user.id)
-        if (trip.userId !== user.id && !isReceiver) throw new Error('Permission denied!')
-
-        // tab天數顯示
-        const dayCount = dayInterval(trip.startDate, trip.endDate)
-        const days = Array.from({ length: dayCount }, (v, i) => i + 1)
-        if (!days.includes(currentDay)) throw new Error("The chosen day doesn't exist!")
-
-        // 找出符合指定day的destination資料
-        Destination.findAll({
-          where: {
-            tripId: id,
-            date: dayAdd(trip.startDate, currentDay - 1).toJSON()
-          },
-          include: Comment,
-          order: [['startTime'], ['endTime']]
-        })
-          .then(destinationsData => {
-            const data = destinationsData.map((destination, i) => ({
-              ...destination.toJSON(),
-              description: destination.toJSON().description.substring(0, 180),
-              count: i + 1
-            }))
-            callback(null, { trip, destinations: data, days, currentDay })
-          })
-          .catch(err => callback(err))
+    try {
+      const tripData = await Trip.findByPk(id, {
+        include: [
+          { model: User, as: 'Sharers' },
+          { model: User, as: 'Receivers' }
+        ]
       })
-      .catch(err => callback(err))
+      if (!tripData) throw new Error("The trip doesn't exist!")
+      const trip = tripData.toJSON()
+      const isReceiver = trip.Receivers.some(r => r.id === user.id)
+      if (trip.userId !== user.id && !isReceiver) throw new Error('Permission denied!')
+
+      // tab天數顯示
+      const dayCount = dayInterval(trip.startDate, trip.endDate)
+      const days = Array.from({ length: dayCount }, (v, i) => i + 1)
+      if (!days.includes(currentDay)) throw new Error("The chosen day doesn't exist!")
+
+      // 找出符合指定day的destination資料
+      const destinationsData = await Destination.findAll({
+        where: {
+          tripId: id,
+          date: dayAdd(trip.startDate, currentDay - 1).toJSON()
+        },
+        include: Comment,
+        order: [['startTime'], ['endTime']]
+      })
+      let data = destinationsData.map((destination, i) => ({
+        ...destination.toJSON(),
+        description: destination.toJSON().description.substring(0, 180),
+        count: i + 1
+      }))
+
+      // 處理Direction API 需求資料
+      if (data.length > 0) {
+        const waypointsForDirection = []
+        for (let i = 1; i < data.length - 1; i++) {
+          waypointsForDirection.push(`${data[i].latitude},${data[i].longitude}`)
+        }
+        const googleMapsParams = {
+          origin: `${data[0].latitude},${data[0].longitude}`,
+          destination: `${data[data.length - 1].latitude},${data[data.length - 1].longitude}`,
+          waypoints: waypointsForDirection,
+          travelMode: 'DRIVING'
+        }
+        const routeResults = await getGoogleMapsRoute(googleMapsParams)
+
+        // 將Direction API 取得的距離與行徑時間資料加入data
+        data = data.map((d, i) => ({
+          ...d,
+          distanceToNext: routeResults.routes[0].legs[i] ? routeResults.routes[0].legs[i].distance.text : null,
+          timeToNext: routeResults.routes[0].legs[i] ? routeResults.routes[0].legs[i].duration.text : null
+        }))
+
+        // 處理前端 Direction.renderer 路徑渲染需求資料
+        const waypointsForRenderer = []
+        for (let i = 1; i < data.length - 1; i++) {
+          waypointsForRenderer.push({ location: { lat: Number(`${data[i].latitude}`), lng: Number(`${data[i].longitude}`) } })
+        }
+        const directionRendererReq = {
+          ...googleMapsParams,
+          waypoints: waypointsForRenderer
+        }
+
+        return callback(null, { trip, destinations: data, days, currentDay, routeResults, directionRendererReq })
+      }
+      return callback(null, { trip, destinations: data, days, currentDay })
+    } catch (err) {
+      callback(err)
+    }
   },
   getSharedTrips: (req, callback) => {
     const user = getUser(req)
